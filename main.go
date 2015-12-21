7b102c603d375b9211ff1f169058bc8a928f0203
xxwei <chaixiaowei2008@qq.com>
2015-12-21 19:07:07 +0800

    tcp server

--- main.go
+++ main.go
@@ -4,27 +4,36 @@ import (
 	"fmt"
 	"gopkg.in/mgo.v2"
 	//"gopkg.in/mgo.v2/bson"
+	"flag"
 	"log"
 	"net"
 	"net/http"
 	"runtime"
+	"strconv"
 	"time"
-	"flag"
 )
 
 func main() {
 	fmt.Println("Stream Server By Golang")
-	fmt.Println("Stream Server is Starting at ",time.Now())
+	fmt.Println("Stream Server is Starting at ", time.Now())
 	time.Sleep(time.Second)
 	var port = flag.Int("port", 9080, "the stream  login auth port")
 	var nbCpus = flag.Int("cpus", runtime.NumCPU()-1, "the cpus to use")
-	var tcpNoDalay = flag.Bool("nodelay", false, "set TCP_NODELAY for connection")
-	flag.Usage = func () {
+	//var tcpNoDalay = flag.Bool("nodelay", false, "set TCP_NODELAY for connection")
+	flag.Usage = func() {
 		fmt.Println("Usage:[--port=int]")
 		fmt.Println("		port,the listen port .default 9080")
 	}
 	flag.Parse()
 	runtime.GOMAXPROCS(*nbCpus)
+	localaddr := ":" + strconv.Itoa(*port)
+	fmt.Println("Server Start At ", localaddr)
+	if tcpaddr, err := net.ResolveTCPAddr("tcp4", fmt.Sprintf(":%v", *port)); err != nil {
+		panic(err)
+	} else {
+		net.ListenTCP("tcp", tcpaddr)
+	}
+
 }
 
 func init() {
@@ -36,7 +45,6 @@ func init() {
 		log.Fatal(err)
 	}
 	defer session.Close()
-	session.
 
 	conn, err := net.Dial("tcp", "www.baidu.com:80")
 	if err != nil {
