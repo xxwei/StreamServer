0144c2110a8d5ca7892d1874cae6f22f1bc454de
xxwei <chaixiaowei2008@qq.com>
2015-12-25 17:36:14 +0800

    file split

--- main.go
+++ main.go
@@ -1,22 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"gopkg.in/mgo.v2"
-	//"gopkg.in/mgo.v2/bson"
 	"flag"
-	"log"
-	"net"
-	"net/http"
+	"fmt"
 	"runtime"
-	"strconv"
 	"time"
 )
 
 func main() {
-	fmt.Println("Stream Server By Golang")
-	fmt.Println("Stream Server is Starting at ", time.Now())
+
+	Welcome()
+	InitMongo()
+	InitHttpServer()
+
 	time.Sleep(time.Second)
+
 	var port = flag.Int("port", 9080, "the stream  login auth port")
 	var nbCpus = flag.Int("cpus", runtime.NumCPU()-1, "the cpus to use")
 	//var tcpNoDalay = flag.Bool("nodelay", false, "set TCP_NODELAY for connection")
@@ -26,35 +24,9 @@ func main() {
 	}
 	flag.Parse()
 	runtime.GOMAXPROCS(*nbCpus)
-	localaddr := ":" + strconv.Itoa(*port)
-	fmt.Println("Server Start At ", localaddr)
-	if tcpaddr, err := net.ResolveTCPAddr("tcp4", fmt.Sprintf(":%v", *port)); err != nil {
-		panic(err)
-	} else {
-		net.ListenTCP("tcp", tcpaddr)
+	InitStreamServer(*port)
+	StartStreamServer()
+	for {
+		time.Sleep(time.Second)
 	}
-}
-
-func init() {
-	fmt.Println("CPU number is", runtime.NumCPU())
-
-	session, err := mgo.Dial("10.192.165.195:20000,10.192.165.196:20000")
-	if err != nil {
-		panic(err)
-		log.Fatal(err)
-	}
-	defer session.Close()
-
-	conn, err := net.Dial("tcp", "www.baidu.com:80")
-	if err != nil {
-		conn.Close()
-	}
-
-}
-func getconfig() {
-	resp, err := http.Get("http://www.baidu.com")
-	if err != nil {
-
-	}
-	defer resp.Body.Close()
 }
